f0375ea1556789a09e42e627ca4b1fcc1a979685
Anurav-Garg <anurav.grg@gmail.com>
2022-11-13 16:30:09 +0530

    Make test route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import { MantineProvider, Text } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
-    </MantineProvider>
+    <BrowserRouter>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MantineProvider>
+    </BrowserRouter>
   );
 }
