289868edb74d0ccee4d1204ad30e134cbbd04f06
pnicto <thehulk489@gmail.com>
2022-11-14 22:59:48 +0530

    Add login route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { MantineProvider } from "@mantine/core";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <MantineProvider withGlobalStyles withNormalizeCSS>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </MantineProvider>
