2af42ad3e19898065f20ee335fc7255235a6f0cb
pnicto <thehulk489@gmail.com>
2022-11-15 21:19:32 +0530

    Remove unused import

--- src/main/java/g9/springframework/silkroad/models/Order.java
+++ src/main/java/g9/springframework/silkroad/models/Order.java
@@ -1,7 +1,5 @@
 package g9.springframework.silkroad.models;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
