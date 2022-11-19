747b9485c830fd993c9fad1277e9d2dfb73e56f3
pnicto <thehulk489@gmail.com>
2022-11-19 10:22:38 +0530

    Finish basic security configuration

---
+++ src/main/java/g9/springframework/silkroad/config/SecurityConfig.java
@@ -0,0 +1,93 @@
+package g9.springframework.silkroad.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import g9.springframework.silkroad.services.CustomUserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+  private final CustomUserDetailsServiceImpl customUserDetailsServiceImpl;
+  private final RsaKeyProperties rsaKeys;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .csrf(csrf -> csrf.disable())
+        .authorizeRequests(auth -> {
+          auth.mvcMatchers("/api/v1/auth/*").permitAll()
+              .mvcMatchers("/api/v1/customers/*").hasAuthority("SCOPE_CUSTOMER")
+              .mvcMatchers("/api/v1/managers/*").hasAuthority("SCOPE_ADMIN");
+          auth.anyRequest().authenticated();
+        })
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        .userDetailsService(customUserDetailsServiceImpl)
+        .build();
+  }
+
+  @Bean
+  public static PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(CustomUserDetailsServiceImpl userDetailsService) {
+    var authProvider = new DaoAuthenticationProvider();
+    authProvider.setPasswordEncoder(passwordEncoder());
+    authProvider.setUserDetailsService(userDetailsService);
+    return new ProviderManager(authProvider);
+  }
+
+  @Bean
+  JwtDecoder jwtDecoder() {
+    System.out.println("being used");
+    return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+  }
+
+  @Bean
+  JwtEncoder jwtEncoder() {
+    JWK kJwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+    JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(kJwk));
+    return new NimbusJwtEncoder(jwkSource);
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}
