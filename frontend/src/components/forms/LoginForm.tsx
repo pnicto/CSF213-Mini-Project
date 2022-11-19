5de8d22518f406380d7b73d14c61d8bc7c476937
pnicto <thehulk489@gmail.com>
2022-11-19 21:54:53 +0530

    Finish login with react query

--- frontend/src/components/forms/LoginForm.tsx
+++ frontend/src/components/forms/LoginForm.tsx
@@ -1,45 +1,77 @@
-import { Title, TextInput, Group, Anchor, Button, Text } from "@mantine/core";
+import { Anchor, Button, Group, Text, TextInput, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import axios from "axios";
 import { useState } from "react";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
+import { useLoginStore } from "../../store/loginStore";
+import { RegisterUserRequestBody } from "../../types/interfaces";
 
 const LoginForm = () => {
   const [pageMode, setPageMode] = useState<"Login" | "Register">("Login");
+  const loginStore = useLoginStore();
+  const navigate = useNavigate();
 
   const form = useForm({
-    initialValues:
-      pageMode === "Login"
-        ? {
-            email: "",
-            password: "",
-            username: "",
-          }
-        : { email: "", password: "" },
-
+    initialValues: {
+      email: "",
+      password: "",
+      name: "",
+      phoneNumber: "",
+    },
     validate: {
       email: (value: string) =>
         /^\S+@\S+$/.test(value) ? null : "Invalid email",
       password: (value: string) =>
-        value.trim().length < 8 ? "Invalid password" : null,
+        value.trim().length < 4 ? "Invalid password" : null,
     },
   });
 
+  const performAuth = useMutation(
+    (requestBody: RegisterUserRequestBody) =>
+      axios.post(
+        `${
+          import.meta.env.VITE_APP_BACKEND_URL
+        }/auth/${pageMode.toLowerCase()}`,
+        requestBody
+      ),
+    {
+      onSuccess: (data) => {
+        loginStore.setAccessToken(data.data.accessToken);
+        navigate("/");
+      },
+    }
+  );
+
   return (
     <form
-      onSubmit={form.onSubmit((values) => console.log(values))}
+      onSubmit={form.onSubmit((values) => {
+        performAuth.mutate(values);
+      })}
       id="login-form"
     >
       <Title order={2} transform="capitalize" color={"deepBlue"} py="md">
         {pageMode}
       </Title>
       {pageMode === "Register" && (
-        <TextInput
-          withAsterisk
-          label="Name"
-          placeholder="John"
-          {...form.getInputProps("username")}
-          type={"text"}
-          py="sm"
-        ></TextInput>
+        <>
+          <TextInput
+            withAsterisk
+            label="Name"
+            placeholder="John"
+            {...form.getInputProps("name")}
+            type={"text"}
+            py="sm"
+          />
+          <TextInput
+            withAsterisk
+            label="Phone Number"
+            placeholder="944143256"
+            {...form.getInputProps("phoneNumber")}
+            type={"text"}
+            py="sm"
+          />
+        </>
       )}
       <TextInput
         withAsterisk
