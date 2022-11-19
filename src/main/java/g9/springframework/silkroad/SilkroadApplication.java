c786aecc84618a5a922e70c3caf2db25c1765df1
pnicto <thehulk489@gmail.com>
2022-11-19 10:15:16 +0530

    Add rsaKey configuration

--- src/main/java/g9/springframework/silkroad/SilkroadApplication.java
+++ src/main/java/g9/springframework/silkroad/SilkroadApplication.java
@@ -2,8 +2,12 @@ package g9.springframework.silkroad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import g9.springframework.silkroad.config.RsaKeyProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SilkroadApplication {
 
 	public static void main(String[] args) {
