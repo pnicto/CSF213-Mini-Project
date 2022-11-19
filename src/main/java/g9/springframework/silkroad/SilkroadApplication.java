9db97c1b100bbbaf28d6f501b9d3065ad603e7e0
pnicto <thehulk489@gmail.com>
2022-11-19 16:33:25 +0530

    Add command line runner for pre populating db

--- src/main/java/g9/springframework/silkroad/SilkroadApplication.java
+++ src/main/java/g9/springframework/silkroad/SilkroadApplication.java
@@ -1,17 +1,35 @@
 package g9.springframework.silkroad;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import g9.springframework.silkroad.config.RsaKeyProperties;
+import g9.springframework.silkroad.models.Admin;
+import g9.springframework.silkroad.models.Manager;
+import g9.springframework.silkroad.repositories.AdminRepository;
+import g9.springframework.silkroad.repositories.ManagerRepository;
+import lombok.AllArgsConstructor;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
-public class SilkroadApplication {
+@AllArgsConstructor
+public class SilkroadApplication implements CommandLineRunner {
+	private final AdminRepository adminRepository;
+	private final ManagerRepository managerRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SilkroadApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		adminRepository.save(new Admin("Abdul", "abdul@email.com", new BCryptPasswordEncoder().encode("password"), "101"));
+
+		managerRepository
+				.save(new Manager("James", "man@email.com", new BCryptPasswordEncoder().encode("bad@pple"), "420"));
 	}
 
 }
