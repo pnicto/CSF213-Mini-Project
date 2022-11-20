ea30f83708e205a4c752553a47655a60a602d482
pnicto <thehulk489@gmail.com>
2022-11-21 01:13:01 +0530

    Use lombok and fix relations

--- src/main/java/g9/springframework/silkroad/models/Category.java
+++ src/main/java/g9/springframework/silkroad/models/Category.java
@@ -1,14 +1,23 @@
 package g9.springframework.silkroad.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Category {
 
   @Id
@@ -16,31 +25,13 @@ public class Category {
   private Long id;
   private String name;
 
-  @OneToMany(mappedBy = "category")
+  @OneToMany(mappedBy = "category", cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH,
+      CascadeType.DETACH })
   private List<Product> products;
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
+  public Category(String name) {
     this.name = name;
-  }
-
-  public List<Product> getProducts() {
-    return products;
-  }
-
-  public void setProducts(List<Product> products) {
-    this.products = products;
+    this.products = new ArrayList<>();
   }
 
 }
