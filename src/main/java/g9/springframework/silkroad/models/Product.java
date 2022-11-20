b03193dd1b5ca4234a5fc1c6095983becf4c4ac1
pnicto <thehulk489@gmail.com>
2022-11-21 01:14:10 +0530

    Fix relations, constructors, expose attr for gson

--- src/main/java/g9/springframework/silkroad/models/Product.java
+++ src/main/java/g9/springframework/silkroad/models/Product.java
@@ -1,10 +1,14 @@
 package g9.springframework.silkroad.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.google.gson.annotations.Expose;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,24 +23,20 @@ public class Product {
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
+  @Expose
   private String name;
+  @Expose
   private String description;
+  @Expose
   private double price;
+  @Expose
   private String imageUrl;
   private int stock;
   private int deliveryTime;
 
-  @ManyToOne
+  @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH })
+  @JoinColumn(name = "category_id")
   private Category category;
-
-  public Product(String name, String description, double price, String imageUrl, int stock, int deliveryTime) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.imageUrl = imageUrl;
-    this.stock = stock;
-    this.deliveryTime = deliveryTime;
-  }
 
   public Product(String name, String description, double price, String imageUrl, int deliveryTime) {
     this.name = name;
@@ -45,6 +45,17 @@ public class Product {
     this.imageUrl = imageUrl;
     this.stock = 1;
     this.deliveryTime = deliveryTime;
+  }
+
+  public Product(String name, String description, double price, String imageUrl, int stock, int deliveryTime,
+      Category category) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.stock = stock;
+    this.deliveryTime = deliveryTime;
+    this.category = category;
   }
 
 }
