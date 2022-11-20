6739043a3c0e951e831f5f94929e06314ea0d945
pnicto <thehulk489@gmail.com>
2022-11-20 14:35:50 +0530

    Finish logout action

--- frontend/src/components/MainNavbar.tsx
+++ frontend/src/components/MainNavbar.tsx
@@ -1,12 +1,48 @@
-import { Container } from "@mantine/core";
+import { ActionIcon, Container, Flex, Menu, Title } from "@mantine/core";
 import { Outlet } from "react-router-dom";
+import { IconDots, IconLogout } from "@tabler/icons";
+import { useLoginStore } from "../store/loginStore";
+
 type Props = {};
 
-const MainNavbar = (props: Props) => {
+const MainNavbar = ({}: Props) => {
+  const loginStore = useLoginStore();
+
   return (
     <>
       <Container fluid mb={"xs"}>
-        <nav></nav>
+        <nav>
+          <Flex justify={"space-between"} p={"xs"}>
+            <div>
+              <Title size="h1" weight="bold" color="deepBlue" order={1}>
+                Silkroad
+              </Title>
+            </div>
+
+            <div>
+              <Menu position="bottom-start">
+                <Menu.Target>
+                  <ActionIcon variant="filled" color={"deepBlue"}>
+                    <IconDots />
+                  </ActionIcon>
+                </Menu.Target>
+
+                <Menu.Dropdown>
+                  <Menu.Item
+                    icon={<IconLogout />}
+                    fw="bold"
+                    color="red"
+                    onClick={() => {
+                      loginStore.logoutUser();
+                    }}
+                  >
+                    Log out
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </div>
+          </Flex>
+        </nav>
       </Container>
       <Outlet />
     </>
