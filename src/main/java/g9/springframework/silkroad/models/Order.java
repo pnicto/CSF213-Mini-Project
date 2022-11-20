90bc9838465c7a248e1969b0ac1d6aa60321c156
pnicto <thehulk489@gmail.com>
2022-11-21 01:13:46 +0530

    Add constructor with gson

--- src/main/java/g9/springframework/silkroad/models/Order.java
+++ src/main/java/g9/springframework/silkroad/models/Order.java
@@ -1,21 +1,38 @@
 package g9.springframework.silkroad.models;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.google.gson.GsonBuilder;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@NoArgsConstructor
 public class Order {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column(columnDefinition = "TEXT")
   private String products;
+
+  public Order(List<Product> products) {
+    var gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .excludeFieldsWithoutExposeAnnotation().create();
+
+    this.products = gson.toJson(products);
+  }
+
 }
