62c5315f40d1789a6e890e0fbb816456dcc9d3d0
pnicto <thehulk489@gmail.com>
2022-11-21 21:08:42 +0530

    Finish home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import { Container, Grid, List, Title } from "@mantine/core";
+import { Container, Grid, List, Skeleton, Title } from "@mantine/core";
 import axios from "axios";
 import { useEffect } from "react";
 import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 import { useLoginStore } from "../store/loginStore";
 import { Category, Product } from "../types/interfaces";
 
@@ -30,8 +31,8 @@ const Home = () => {
   }, [isLogged]);
 
   return (
-    <Container fluid px={8}>
-      <Grid p={0} columns={14}>
+    <Container fluid>
+      <Grid p={0} columns={13}>
         <Grid.Col span={2}>
           <Container pos={"fixed"}>
             <Title pb="xs" order={2} color={"deepBlue"}>
@@ -45,16 +46,16 @@ const Home = () => {
           </Container>
         </Grid.Col>
         <Grid.Col
-          span={12}
+          span={11}
           style={{
             borderLeft: "solid 1px #9DA5C0",
           }}
         >
-          <Grid columns={3}>
+          <Grid columns={4} gutter={"lg"} mx={"xs"}>
             {productsQuery.data?.data.map((product) => {
               return (
                 <Grid.Col key={product.id} span={1}>
-                  {product.name}
+                  <ProductCard key={product.id} product={product} />
                 </Grid.Col>
               );
             })}
