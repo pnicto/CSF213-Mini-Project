5762f1a8271671fc5b95d0b5d1002930ab6899d8
pnicto <thehulk489@gmail.com>
2022-11-21 16:28:08 +0530

    Add categories fetch, Add comments

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,16 +5,22 @@ import { useNavigate } from "react-router-dom";
 import { useLoginStore } from "../store/loginStore";
 
 const Home = () => {
-  const { isLogged } = useLoginStore();
+  // Hooks
+  const { isLogged, accessToken } = useLoginStore();
   const navigate = useNavigate();
 
-  const { accessToken } = useLoginStore();
+  // Axios default header setting
   axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 
-  const { data, isLoading } = useQuery("products", () =>
+  // Queries for data
+  const productsQuery = useQuery("products", () =>
     axios.get(`${import.meta.env.VITE_APP_BACKEND_URL}/products`)
   );
+  const categoriesQuery = useQuery("categories", () =>
+    axios.get(`${import.meta.env.VITE_APP_BACKEND_URL}/categories`)
+  );
 
+  // Navigate to login route if the user is not logged in
   useEffect(() => {
     if (!isLogged) {
       navigate("/login");
