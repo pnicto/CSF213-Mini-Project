3272ed7223fa76398a03e8429d34642c2fae335c
pnicto <thehulk489@gmail.com>
2022-11-21 16:06:16 +0530

    Finish products data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,19 @@
+import axios from "axios";
 import { useEffect } from "react";
+import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { useLoginStore } from "../store/loginStore";
 
 const Home = () => {
   const { isLogged } = useLoginStore();
   const navigate = useNavigate();
+
+  const { accessToken } = useLoginStore();
+  axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  const { data, isLoading } = useQuery("products", () =>
+    axios.get(`${import.meta.env.VITE_APP_BACKEND_URL}/products`)
+  );
 
   useEffect(() => {
     if (!isLogged) {
