b24a133320a8e442e78efc1445d75afb01ef7800
pnicto <thehulk489@gmail.com>
2022-11-21 21:08:36 +0530

    Finish product card

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,7 +1,46 @@
-type Props = {};
+import { Card, Group, Image, Text, Badge } from "@mantine/core";
+import { Link } from "react-router-dom";
+import { Product } from "../types/interfaces";
 
-const ProductCard = ({}: Props) => {
-  return <div>ProductCard</div>;
+type Props = {
+  product: Product;
+};
+
+const ProductCard = ({ product }: Props) => {
+  const { name, imageUrl, isAvailable, price } = product;
+
+  return (
+    <Card
+      shadow={"md"}
+      withBorder
+      h={"100%"}
+      component={Link}
+      to={`/product/${product.id}`}
+    >
+      <Card.Section withBorder>
+        <Image
+          src={imageUrl}
+          alt={name}
+          height={160}
+          fit="contain"
+          withPlaceholder
+        />
+      </Card.Section>
+      <Group position="apart" mt="md" mb="xs">
+        <Text weight={300}>{price}</Text>
+        {isAvailable ? (
+          <Badge color="green" variant="light">
+            In stock
+          </Badge>
+        ) : (
+          <Badge color="red" variant="light">
+            Out of stock
+          </Badge>
+        )}
+      </Group>
+      <Text weight={400}>{name}</Text>
+    </Card>
+  );
 };
 
 export default ProductCard;
