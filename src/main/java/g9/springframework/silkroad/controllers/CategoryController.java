95592ec6d5471c1585d48fcf15dbc5ea6ba9ef8a
pnicto <thehulk489@gmail.com>
2022-11-21 16:17:27 +0530

    Init category controller

---
+++ src/main/java/g9/springframework/silkroad/controllers/CategoryController.java
@@ -0,0 +1,17 @@
+package g9.springframework.silkroad.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import g9.springframework.silkroad.models.Category;
+import g9.springframework.silkroad.repositories.CategoryRepository;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/categories")
+public class CategoryController {
+  private final CategoryRepository categoryRepository;
+
+}
