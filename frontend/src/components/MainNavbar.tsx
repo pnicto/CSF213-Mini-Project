baa8e83a92b860504af9be637169dc816d727249
pnicto <thehulk489@gmail.com>
2022-11-22 21:25:22 +0530

    Fix routing for navbar links

--- frontend/src/components/MainNavbar.tsx
+++ frontend/src/components/MainNavbar.tsx
@@ -37,7 +37,7 @@ const MainNavbar = ({ navbarElements = ["cart", "my profile"] }: Props) => {
                   fw="bold"
                   key={idx}
                   component={Link}
-                  to={`/${ele}`}
+                  to={`/app/${ele}`}
                   tt="capitalize"
                   size="lg"
                 >
