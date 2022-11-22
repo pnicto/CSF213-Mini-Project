7b2f36972e03a143150446855804bdde1d2fa522
pnicto <thehulk489@gmail.com>
2022-11-22 13:02:04 +0530

    Finish product details fetch

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { Product } from "../types/interfaces";
 
 const ProductDetails = () => {
   const { productId } = useParams<{ productId?: string }>();
+
+  const productQuery = useQuery(["products", productId], () =>
+    axios.get<Product>(
+      `${import.meta.env.VITE_APP_BACKEND_URL}/products/${productId}`
+    )
+  );
 
   return <div>ProductDetails</div>;
 };
