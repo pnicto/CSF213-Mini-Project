55e557af388a54cca57d0efe3f222ac431954a28
pnicto <thehulk489@gmail.com>
2022-11-22 13:08:42 +0530

    Add loading spinner when loading

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { Center, Loader } from "@mantine/core";
 import axios from "axios";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
@@ -6,13 +7,21 @@ import { Product } from "../types/interfaces";
 const ProductDetails = () => {
   const { productId } = useParams<{ productId?: string }>();
 
-  const productQuery = useQuery(["products", productId], () =>
-    axios.get<Product>(
-      `${import.meta.env.VITE_APP_BACKEND_URL}/products/${productId}`
-    )
+  const { data: productData, isLoading } = useQuery(
+    ["products", productId],
+    () =>
+      axios.get<Product>(
+        `${import.meta.env.VITE_APP_BACKEND_URL}/products/${productId}`
+      )
   );
 
-  return <div>ProductDetails</div>;
+  return isLoading ? (
+    <Center h={"80vh"}>
+      <Loader size={"md"} />
+    </Center>
+  ) : (
+    <div>{productData?.data.name}</div>
+  );
 };
 
 export default ProductDetails;
