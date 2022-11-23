a8cae45ea23fd89ca7ccacf6d9890a03652cba10
pnicto <thehulk489@gmail.com>
2022-11-23 17:55:59 +0530

    Init cart controller

---
+++ src/main/java/g9/springframework/silkroad/controllers/CartController.java
@@ -0,0 +1,13 @@
+package g9.springframework.silkroad.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/customers/cart")
+public class CartController {
+
+}
