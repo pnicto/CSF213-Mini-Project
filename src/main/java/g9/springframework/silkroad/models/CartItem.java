c766e934a60dd364c0e21f82c5af4fe356129031
pnicto <thehulk489@gmail.com>
2022-11-23 12:43:44 +0530

    Finish cart item

---
+++ src/main/java/g9/springframework/silkroad/models/CartItem.java
@@ -0,0 +1,32 @@
+package g9.springframework.silkroad.models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class CartItem {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  private Product product;
+  private Integer quantity;
+
+  public CartItem(Product product, Integer quantity) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+
+}
