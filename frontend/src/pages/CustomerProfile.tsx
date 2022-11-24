f2a6e7ed71eaed5d638681bebdf6ce896efd85bb
pnicto <thehulk489@gmail.com>
2022-11-24 17:21:11 +0530

    Add query for profile data

--- frontend/src/pages/CustomerProfile.tsx
+++ frontend/src/pages/CustomerProfile.tsx
@@ -1,4 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Customer } from "../types/interfaces";
+
 const CustomerProfile = () => {
+  const profileDataQuery = useQuery(["customerProfile"], () =>
+    axios.get<Customer>(`${import.meta.env.VITE_APP_BACKEND_URL}/customers`)
+  );
+
   return <div>CustomerProfile</div>;
 };
 
