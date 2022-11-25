527facbe426fc509b62b587569b039b623f58418
pnicto <thehulk489@gmail.com>
2022-11-25 18:07:24 +0530

    Finish change password modal

--- frontend/src/components/modals/ChangePassword.tsx
+++ frontend/src/components/modals/ChangePassword.tsx
@@ -1,7 +1,85 @@
+import { Button, PasswordInput, Stack } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { useNotificationStore } from "../../store/notificationStore";
+
 type Props = {};
 
 const ChangePassword = ({}: Props) => {
-  return <div>ChangePassword</div>;
+  const notificationStore = useNotificationStore();
+
+  const passwordForm = useForm({
+    initialValues: {
+      oldPassword: "",
+      newPassword: "",
+      confirmNewPassword: "",
+    },
+    validate: {
+      oldPassword: (value: string) =>
+        value.trim().length < 4 ? "Password is too short" : null,
+      newPassword: (value: string) =>
+        value.trim().length < 4 ? "Password is too short" : null,
+    },
+  });
+
+  const updateProfile = useMutation(
+    (requestBody: { oldPassword: string; newPassword: string }) => {
+      return axios.patch(
+        `${import.meta.env.VITE_APP_BACKEND_URL}/customers/changePassword`,
+        requestBody
+      );
+    },
+    {
+      onSuccess: (data) => {
+        notificationStore.successNotification("Password changed successfully!");
+      },
+      onError: (data: AxiosError) => {
+        notificationStore.errorNotification(
+          data.message,
+          `Cannot change password`
+        );
+      },
+    }
+  );
+  return (
+    <>
+      <form
+        onSubmit={passwordForm.onSubmit((values) => {
+          if (values.confirmNewPassword === values.newPassword) {
+            updateProfile.mutate({
+              oldPassword: values.oldPassword,
+              newPassword: values.newPassword,
+            });
+          } else {
+            notificationStore.errorNotification("New password does not match");
+          }
+        })}
+      >
+        <Stack>
+          <PasswordInput
+            withAsterisk
+            placeholder="Old password"
+            label="Old password"
+            {...passwordForm.getInputProps("oldPassword")}
+          />
+          <PasswordInput
+            withAsterisk
+            placeholder="New password"
+            label="New password"
+            {...passwordForm.getInputProps("newPassword")}
+          />
+          <PasswordInput
+            withAsterisk
+            placeholder="Confirm new password"
+            label="Confirm new password"
+            {...passwordForm.getInputProps("confirmNewPassword")}
+          />
+          <Button type="submit">Submit</Button>
+        </Stack>
+      </form>
+    </>
+  );
 };
 
 export default ChangePassword;
