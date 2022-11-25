4d24eb3fcb52e575ce0fa9abad735d202bb2580c
pnicto <thehulk489@gmail.com>
2022-11-25 14:37:39 +0530

    Finish order items container

--- frontend/src/components/display/orders/OrderItemsContainer.tsx
+++ frontend/src/components/display/orders/OrderItemsContainer.tsx
@@ -1,11 +1,28 @@
+import { Flex, Paper, Text } from "@mantine/core";
 import { Order } from "../../../types/interfaces";
 
 type Props = {
   order: Order;
 };
 
-const OrderItemsContainer = ({}: Props) => {
-  return <div>OrderItemsContainer</div>;
+const OrderItemsContainer = ({ order }: Props) => {
+  const orderDate = new Date(order.createdAt);
+
+  return (
+    <Paper withBorder shadow={"md"} p={"xl"}>
+      <Flex justify={"space-around"}>
+        <Text size={"lg"}>
+          <strong>Id:</strong> #{order.id}
+        </Text>
+        <Text size={"lg"}>
+          <strong>Ordered at:</strong> {orderDate.toLocaleString()}
+        </Text>
+        <Text size={"lg"}>
+          <strong>Total price:</strong> &#8377;{order.totalPrice}
+        </Text>
+      </Flex>
+    </Paper>
+  );
 };
 
 export default OrderItemsContainer;
