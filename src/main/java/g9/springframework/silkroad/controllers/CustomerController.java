747e5862f44ada12fe9e34b1e825745f6a9c26e5
Anurav-Garg <anurav.grg@gmail.com>
2022-11-25 16:28:34 +0530

    Add route for updating customer

--- src/main/java/g9/springframework/silkroad/controllers/CustomerController.java
+++ src/main/java/g9/springframework/silkroad/controllers/CustomerController.java
@@ -4,6 +4,8 @@ import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,20 @@ public class CustomerController {
   Customer getCustomer(Principal principal) {
     Optional<Customer> cOptional = customerRepository.findByEmail(principal.getName());
     if (cOptional.isPresent()) {
+      return cOptional.get();
+    } else {
+      throw new IllegalStateException("Customer not found");
+    }
+  }
+
+  @PatchMapping
+  Customer updateCustomer(@RequestBody Customer updatedCustomer) {
+    Optional<Customer> cOptional = customerRepository.findById(updatedCustomer.getId());
+    if (cOptional.isPresent()) {
+      cOptional.get().setName(updatedCustomer.getName());
+      cOptional.get().setPhoneNumber(updatedCustomer.getPhoneNumber());
+      cOptional.get().setMoneyInWallet(updatedCustomer.getMoneyInWallet());
+      customerRepository.save(cOptional.get());
       return cOptional.get();
     } else {
       throw new IllegalStateException("Customer not found");
