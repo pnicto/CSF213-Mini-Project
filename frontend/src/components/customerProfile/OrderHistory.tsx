224561eb5172ed4a0bd92da98acdc1659f9f3cd7
pnicto <thehulk489@gmail.com>
2022-11-25 14:00:24 +0530

    Add loading spinner

--- frontend/src/components/customerProfile/OrderHistory.tsx
+++ frontend/src/components/customerProfile/OrderHistory.tsx
@@ -1,7 +1,26 @@
-type Props = {};
+import { Center, Loader } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Customer } from "../../types/interfaces";
 
-const OrderHistory = (props: Props) => {
-  return <div>OrderHistory</div>;
+const OrderHistory = () => {
+  const profileDataQuery = useQuery(["customerProfile"], () =>
+    axios.get<Customer>(`${import.meta.env.VITE_APP_BACKEND_URL}/customers`)
+  );
+
+  if (profileDataQuery.isLoading) {
+    return (
+      <Center h={"80vh"}>
+        <Loader size={"md"} />
+      </Center>
+    );
+  }
+
+  return (
+    <>
+      <div>Order history</div>
+    </>
+  );
 };
 
 export default OrderHistory;
