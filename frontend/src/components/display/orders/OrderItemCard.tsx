e8522d744e592e35c1d2580278e31bf9d1d94adb
pnicto <thehulk489@gmail.com>
2022-11-25 23:06:45 +0530

    Use grids instead of flex
    Removed unnecessary containers too

--- frontend/src/components/display/orders/OrderItemCard.tsx
+++ frontend/src/components/display/orders/OrderItemCard.tsx
@@ -1,12 +1,4 @@
-import {
-  Anchor,
-  Container,
-  Flex,
-  Image,
-  Stack,
-  Text,
-  Title
-} from "@mantine/core";
+import { Anchor, Grid, Image, Stack, Text, Title } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { OrderItem } from "../../../types/interfaces";
 
@@ -17,40 +9,33 @@ type Props = {
 const OrderItemCard = ({ orderItem }: Props) => {
   const { name, price, quantity, imageUrl, productId } = orderItem;
   return (
-    <Container>
-      <Flex direction={"row"} justify="space-between" align={"center"}>
-        <Image
-          alt={name}
-          src={imageUrl}
-          height={150}
-          width={150}
-          fit="contain"
-          m={"lg"}
-        />
-        <Container maw={"40%"}>
-          <Stack>
-            <Anchor component={Link} to={`/app/product/${productId}`}>
-              <Title order={4}>{name}</Title>
-            </Anchor>
+    <>
+      <Grid.Col span={1}>
+        <Image alt={name} src={imageUrl} height={150} fit="contain" m={"md"} />
+      </Grid.Col>
+      <Grid.Col span={2}>
+        <Stack>
+          <Anchor component={Link} to={`/app/product/${productId}`}>
+            <Title order={4}>{name}</Title>
+          </Anchor>
+          <div>
+            <Text>&#8377; {price}</Text>
             <div>
-              <Text>&#8377; {price}</Text>
-              <div>
-                <Text span>Quantity: </Text>
-                <Text span color={"blue"}>
-                  {quantity}
-                </Text>
-              </div>
-              <div>
-                <Text span>Total price: </Text>
-                <Text span color={"blue"}>
-                  {(quantity * price).toFixed(2)}
-                </Text>
-              </div>
+              <Text span>Quantity: </Text>
+              <Text span color={"blue"}>
+                {quantity}
+              </Text>
             </div>
-          </Stack>
-        </Container>
-      </Flex>
-    </Container>
+            <div>
+              <Text span>Total price: </Text>
+              <Text span color={"blue"}>
+                {(quantity * price).toFixed(2)}
+              </Text>
+            </div>
+          </div>
+        </Stack>
+      </Grid.Col>
+    </>
   );
 };
 
