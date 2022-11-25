975bc31f436cb0868f55b55173477d9a43f4397a
pnicto <thehulk489@gmail.com>
2022-11-25 18:05:49 +0530

    Finish change password route

--- src/main/java/g9/springframework/silkroad/controllers/CustomerController.java
+++ src/main/java/g9/springframework/silkroad/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package g9.springframework.silkroad.controllers;
 import java.security.Principal;
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CustomerController {
   private final CustomerRepository customerRepository;
+  private final PasswordEncoder passwordEncoder;
 
   @GetMapping
   Customer getCustomer(Principal principal) {
@@ -43,4 +45,22 @@ public class CustomerController {
       throw new IllegalStateException("Customer not found");
     }
   }
+
+  @PatchMapping("/changePassword")
+  Customer changeCustomerPassword(@RequestBody ChangePasswordBody requestBody, Principal principal) {
+    Optional<Customer> cOptional = customerRepository.findByEmail(principal.getName());
+    boolean isCorrectPassword = passwordEncoder.matches(requestBody.oldPassword(), cOptional.get().getPassword());
+
+    if (cOptional.isPresent() && isCorrectPassword) {
+      Customer customer = cOptional.get();
+      customer.setPassword(passwordEncoder.encode(requestBody.newPassword()));
+      return customerRepository.save(customer);
+    } else {
+      throw new IllegalStateException("Customer not found");
+    }
+  }
+}
+
+record ChangePasswordBody(
+    String oldPassword, String newPassword) {
 }
