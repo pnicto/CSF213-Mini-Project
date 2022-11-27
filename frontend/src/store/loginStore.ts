29126df465448861a7c6b2db98377d6f1ad088ec
pnicto <thehulk489@gmail.com>
2022-11-27 14:08:02 +0530

    Create set authority method in store

--- frontend/src/store/loginStore.ts
+++ frontend/src/store/loginStore.ts
@@ -1,10 +1,12 @@
 import create from "zustand";
-import { LoginStore } from "../types/interfaces";
+import { Authority, LoginStore } from "../types/interfaces";
 
 export const useLoginStore = create<LoginStore>()((set) => ({
   accessToken: null,
   isLogged: false,
+  authority: null,
   setAccessToken: (accessToken: string) =>
     set((state) => ({ accessToken, isLogged: true })),
   logoutUser: () => set((state) => ({ accessToken: null, isLogged: false })),
+  setAuthority: (authority: Authority) => set((state) => ({ authority })),
 }));
