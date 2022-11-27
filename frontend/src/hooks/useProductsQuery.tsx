a6b37e5f16ef546da9d79a9ae28941e19ba69d23
pnicto <thehulk489@gmail.com>
2022-11-27 21:23:55 +0530

    Add enabled bool in func params

--- frontend/src/hooks/useProductsQuery.tsx
+++ frontend/src/hooks/useProductsQuery.tsx
@@ -2,12 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Category, Product } from "../types/interfaces";
 
-export const useProductQuery = (activeCategory: Category | null) =>
-  useQuery(["products", activeCategory], () =>
-    axios.get<Product[]>(
-      // If no category is selected set the request param to 0 to get all products
-      `${import.meta.env.VITE_APP_BACKEND_URL}/products?name=${
-        activeCategory ? activeCategory.id : 0
-      }`
-    )
+export const useProductQuery = (
+  activeCategory: Category | null,
+  enabled: boolean = true
+) =>
+  useQuery(
+    ["products", activeCategory],
+    () =>
+      axios.get<Product[]>(
+        // If no category is selected set the request param to 0 to get all products
+        `${import.meta.env.VITE_APP_BACKEND_URL}/products?name=${
+          activeCategory ? activeCategory.id : 0
+        }`
+      ),
+    {
+      enabled,
+    }
   );
