5f4f2f5eca2e2f8fce74707173ec3c1b96228742
pnicto <thehulk489@gmail.com>
2022-11-27 16:04:59 +0530

    Add delete product route

--- src/main/java/g9/springframework/silkroad/controllers/ProductController.java
+++ src/main/java/g9/springframework/silkroad/controllers/ProductController.java
@@ -2,6 +2,7 @@ package g9.springframework.silkroad.controllers;
 
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +46,11 @@ public class ProductController {
   @PostMapping
   public Product createNewProduct(@RequestBody Product newProduct) {
     return productRepository.save(newProduct);
+  }
+
+  @DeleteMapping("/{id}")
+  public Iterable<Product> deleteProduct(@PathVariable("id") Long id) {
+    productRepository.deleteById(id);
+    return productRepository.findAll();
   }
 }
