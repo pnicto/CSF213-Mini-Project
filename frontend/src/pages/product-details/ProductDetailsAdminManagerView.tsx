da7fdea3b26f11b910d21bddeb06a6bf68f0a33a
pnicto <thehulk489@gmail.com>
2022-11-28 10:09:28 +0530

    Create form fields for edits

--- frontend/src/pages/product-details/ProductDetailsAdminManagerView.tsx
+++ frontend/src/pages/product-details/ProductDetailsAdminManagerView.tsx
@@ -1,3 +1,15 @@
+import {
+  Button,
+  Grid,
+  Image,
+  NumberInput,
+  Stack,
+  Switch,
+  Textarea,
+  TextInput,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useState } from "react";
 import { Product } from "../../types/interfaces";
 
 type Props = {
@@ -5,7 +17,81 @@ type Props = {
 };
 
 const ProductDetailsAdminManagerView = ({ product }: Props) => {
-  return <div>ProductDetailsAdminManagerView</div>;
+  const { name, description, price, imageUrl, deliveryTime } = product;
+  const [isAvailable, setIsAvailable] = useState(product.isAvailable);
+
+  const form = useForm({
+    initialValues: {
+      name,
+      description,
+      imageUrl,
+      price,
+      deliveryTime,
+    },
+  });
+
+  return (
+    <>
+      <Grid.Col span={3} offset={2}>
+        <TextInput
+          withAsterisk
+          required
+          label="Link to image"
+          value={imageUrl}
+          {...form.getInputProps("imageUrl")}
+        />
+        <Image
+          alt={name}
+          src={imageUrl}
+          withPlaceholder
+          height={500}
+          fit="contain"
+        />
+      </Grid.Col>
+      <Grid.Col span={7} offset={1}>
+        <Stack align={"flex-start"}>
+          <TextInput
+            withAsterisk
+            required
+            label="Name"
+            {...form.getInputProps("name")}
+            w={"40%"}
+          />
+          <Switch
+            required
+            label="Is in stock?"
+            checked={isAvailable}
+            onChange={(event) => setIsAvailable(event.currentTarget.checked)}
+          />
+          <NumberInput
+            withAsterisk
+            required
+            precision={2}
+            label="Price in &#8377;"
+            min={1}
+            step={50}
+            {...form.getInputProps("price")}
+          />
+          <NumberInput
+            withAsterisk
+            required
+            label="Delivery time in days"
+            min={1}
+            {...form.getInputProps("deliveryTime")}
+          />
+          <Textarea
+            withAsterisk
+            w={"70%"}
+            required
+            autosize
+            label="Description"
+            {...form.getInputProps("description")}
+          />
+          <Button onClick={() => {}}>Apply edits</Button>
+        </Stack>
+      </Grid.Col>
+    </>
+  );
 };
 
 export default ProductDetailsAdminManagerView;
