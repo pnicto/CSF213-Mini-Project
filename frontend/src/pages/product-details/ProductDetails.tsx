12bf5e70865507aa5734137e055c609618ec47d1
pnicto <thehulk489@gmail.com>
2022-11-28 11:15:31 +0530

    Remove queries, spinners

--- frontend/src/pages/product-details/ProductDetails.tsx
+++ frontend/src/pages/product-details/ProductDetails.tsx
@@ -1,10 +1,6 @@
 import { Center, Grid } from "@mantine/core";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { useParams } from "react-router-dom";
-import LoadingSpinner from "../../components/display/LoadingSpinner";
 import { useLoginStore } from "../../store/useLoginStore";
-import { Product } from "../../types/interfaces";
 import ProductDetailsAdminManagerView from "./ProductDetailsAdminManagerView";
 import ProductDetailsCustomerView from "./ProductDetailsCustomerView";
 
@@ -13,35 +9,23 @@ const ProductDetails = () => {
   const { productId } = useParams<{ productId?: string }>();
   const { authority } = useLoginStore();
 
-  const { data: productData, isLoading } = useQuery(
-    ["products", productId],
-    () =>
-      axios.get<Product>(
-        `${import.meta.env.VITE_APP_BACKEND_URL}/products/${productId}`
-      )
-  );
-
-  if (isLoading) {
-    return <LoadingSpinner />;
-  } else {
-    return (
-      <>
-        {authority === "CUSTOMER" && (
-          <Center mih={"65vh"}>
-            <Grid columns={13} maw={"100vw"} m={0}>
-              <ProductDetailsCustomerView product={productData!.data} />
-            </Grid>
-          </Center>
-        )}
-
-        {authority !== "CUSTOMER" && (
-          <Grid columns={13} m={0}>
-            <ProductDetailsAdminManagerView product={productData!.data} />
+  return (
+    <>
+      {authority === "CUSTOMER" && (
+        <Center mih={"65vh"}>
+          <Grid columns={13} maw={"100vw"} m={0}>
+            <ProductDetailsCustomerView productId={productId!} />
           </Grid>
-        )}
-      </>
-    );
-  }
+        </Center>
+      )}
+
+      {authority !== "CUSTOMER" && (
+        <Grid columns={13} m={0}>
+          <ProductDetailsAdminManagerView productId={productId!} />
+        </Grid>
+      )}
+    </>
+  );
 };
 
 export default ProductDetails;
