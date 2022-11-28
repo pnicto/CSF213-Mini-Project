a16500ac82fec5d4807d1924f249b0a95b31b34f
pnicto <thehulk489@gmail.com>
2022-11-28 09:38:18 +0530

    Fix typo in file name

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -12,7 +12,7 @@ import { useMutation } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
 import LoadingSpinner from "../components/display/LoadingSpinner";
 import ProductCard from "../components/display/ProductCard";
-import ProductFrom from "../components/forms/AddProductFrom";
+import ProductFrom from "../components/forms/AddProductForm";
 import { useCategoriesQuery } from "../hooks/useCategoriesQuery";
 import { useProductQuery } from "../hooks/useProductsQuery";
 import { useActiveCategoryStore } from "../store/useActiveCategoryStore";
