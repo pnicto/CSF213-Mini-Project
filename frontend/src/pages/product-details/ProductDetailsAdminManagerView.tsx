dbba0375cfc4a7dc9a16677c110fca5c95249e86
pnicto <thehulk489@gmail.com>
2022-11-28 10:43:52 +0530

    Finish updating product

--- frontend/src/pages/product-details/ProductDetailsAdminManagerView.tsx
+++ frontend/src/pages/product-details/ProductDetailsAdminManagerView.tsx
@@ -9,26 +9,60 @@ import {
   TextInput,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import { useState } from "react";
+import { useNotificationStore } from "../../store/useNotificationStore";
 import { Product } from "../../types/interfaces";
 
 type Props = {
   product: Product;
 };
 
+export interface UpdateProductRequest {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  isAvailable: boolean;
+  deliveryTime: number;
+}
+
 const ProductDetailsAdminManagerView = ({ product }: Props) => {
-  const { name, description, price, imageUrl, deliveryTime } = product;
+  const { name, description, price, deliveryTime } = product;
   const [isAvailable, setIsAvailable] = useState(product.isAvailable);
+  const [imageUrl, setImageUrl] = useState(product.imageUrl);
+  const notificationStore = useNotificationStore();
 
   const form = useForm({
     initialValues: {
       name,
       description,
-      imageUrl,
       price,
       deliveryTime,
     },
   });
+
+  const updateProduct = useMutation(
+    (requestBody: UpdateProductRequest) => {
+      return axios.patch<Product>(
+        `${import.meta.env.VITE_APP_BACKEND_URL}/products/${product.id}`,
+        requestBody
+      );
+    },
+    {
+      onSuccess: () => {
+        notificationStore.successNotification("Product updated successfully");
+      },
+      onError: (data: AxiosError) => {
+        notificationStore.errorNotification(
+          data.message,
+          `Could not update product details`
+        );
+      },
+    }
+  );
 
   return (
     <>
@@ -38,7 +72,7 @@ const ProductDetailsAdminManagerView = ({ product }: Props) => {
           required
           label="Link to image"
           value={imageUrl}
-          {...form.getInputProps("imageUrl")}
+          onChange={(event) => setImageUrl(event.currentTarget.value)}
         />
         <Image
           alt={name}
@@ -49,46 +83,56 @@ const ProductDetailsAdminManagerView = ({ product }: Props) => {
         />
       </Grid.Col>
       <Grid.Col span={7} offset={1}>
-        <Stack align={"flex-start"}>
-          <TextInput
-            withAsterisk
-            required
-            label="Name"
-            {...form.getInputProps("name")}
-            w={"40%"}
-          />
-          <Switch
-            required
-            label="Is in stock?"
-            checked={isAvailable}
-            onChange={(event) => setIsAvailable(event.currentTarget.checked)}
-          />
-          <NumberInput
-            withAsterisk
-            required
-            precision={2}
-            label="Price in &#8377;"
-            min={1}
-            step={50}
-            {...form.getInputProps("price")}
-          />
-          <NumberInput
-            withAsterisk
-            required
-            label="Delivery time in days"
-            min={1}
-            {...form.getInputProps("deliveryTime")}
-          />
-          <Textarea
-            withAsterisk
-            w={"70%"}
-            required
-            autosize
-            label="Description"
-            {...form.getInputProps("description")}
-          />
-          <Button onClick={() => {}}>Apply edits</Button>
-        </Stack>
+        <form
+          onSubmit={form.onSubmit((values) => {
+            updateProduct.mutate({
+              ...values,
+              id: product.id,
+              isAvailable,
+              imageUrl,
+            });
+          })}
+        >
+          <Stack align={"flex-start"}>
+            <TextInput
+              withAsterisk
+              required
+              label="Name"
+              {...form.getInputProps("name")}
+              w={"40%"}
+            />
+            <Switch
+              label="Is in stock?"
+              checked={isAvailable}
+              onChange={(event) => setIsAvailable(event.currentTarget.checked)}
+            />
+            <NumberInput
+              withAsterisk
+              required
+              precision={2}
+              label="Price in &#8377;"
+              min={1}
+              step={50}
+              {...form.getInputProps("price")}
+            />
+            <NumberInput
+              withAsterisk
+              required
+              label="Delivery time in days"
+              min={1}
+              {...form.getInputProps("deliveryTime")}
+            />
+            <Textarea
+              withAsterisk
+              w={"70%"}
+              required
+              autosize
+              label="Description"
+              {...form.getInputProps("description")}
+            />
+            <Button type="submit">Apply edits</Button>
+          </Stack>
+        </form>
       </Grid.Col>
     </>
   );
