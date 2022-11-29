3d556e1a7d6dc40d239438094d51344e0a12187c
pnicto <thehulk489@gmail.com>
2022-11-29 14:41:00 +0530

    Init admin controller

---
+++ src/main/java/g9/springframework/silkroad/controllers/AdminController.java
@@ -0,0 +1,13 @@
+package g9.springframework.silkroad.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/admins")
+@AllArgsConstructor
+public class AdminController {
+
+}
