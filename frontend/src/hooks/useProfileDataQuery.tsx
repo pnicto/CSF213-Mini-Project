fada8db7e84f819c9a8c4c29b3ef7c03e5ccddc1
pnicto <thehulk489@gmail.com>
2022-11-29 14:13:49 +0530

    Make profile data query conditional

--- frontend/src/hooks/useProfileDataQuery.tsx
+++ frontend/src/hooks/useProfileDataQuery.tsx
@@ -1,8 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useLoginStore } from "../store/useLoginStore";
 import { Customer } from "../types/interfaces";
 
-export const useProfileDataQuery = () =>
-  useQuery(["customerProfile"], () =>
-    axios.get<Customer>(`${import.meta.env.VITE_APP_BACKEND_URL}/customers`)
+export const useProfileDataQuery = () => {
+  const { authority } = useLoginStore();
+  let queryKeys: string[];
+  switch (authority) {
+    case "CUSTOMER":
+      queryKeys = ["customerProfile"];
+      break;
+    case "MANAGER":
+      queryKeys = ["managerProfile"];
+      break;
+    default:
+      queryKeys = ["adminProfile"];
+  }
+  return useQuery(queryKeys, () =>
+    axios.get<Customer>(
+      `${import.meta.env.VITE_APP_BACKEND_URL}/${authority?.toLowerCase()}s`
+    )
   );
+};
