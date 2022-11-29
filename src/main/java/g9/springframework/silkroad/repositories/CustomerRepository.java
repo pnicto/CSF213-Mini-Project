cf9c0ae91324a8a5abe42a39b290f22d39a18c0f
pnicto <thehulk489@gmail.com>
2022-11-29 13:26:33 +0530

    Make customer repo transactional

--- src/main/java/g9/springframework/silkroad/repositories/CustomerRepository.java
+++ src/main/java/g9/springframework/silkroad/repositories/CustomerRepository.java
@@ -3,9 +3,11 @@ package g9.springframework.silkroad.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import g9.springframework.silkroad.models.Customer;
 
+@Transactional
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
   Optional<Customer> findByEmail(String email);
 
