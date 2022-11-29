6e8170ff744e54b225d9ef7da83100e15a3b9ea9
pnicto <thehulk489@gmail.com>
2022-11-29 14:34:44 +0530

    Create patch route for manager

--- src/main/java/g9/springframework/silkroad/controllers/ManagerController.java
+++ src/main/java/g9/springframework/silkroad/controllers/ManagerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,6 +39,20 @@ public class ManagerController {
   @PostMapping
   Manager createNewManager(@RequestBody Manager newManager) {
     return managerRepository.save(newManager);
+  }
+
+  @PatchMapping
+  Manager updateManager(@RequestBody Manager updatedManager, Principal principal) {
+    Optional<Manager> cOptional = managerRepository.findByEmail(principal.getName());
+    if (cOptional.isPresent()) {
+      Manager manager = cOptional.get();
+      manager.setName(updatedManager.getName());
+      manager.setPhoneNumber(updatedManager.getPhoneNumber());
+      managerRepository.save(manager);
+      return manager;
+    } else {
+      throw new IllegalStateException("Manager not found");
+    }
   }
 
   @DeleteMapping("/{managerId}")
