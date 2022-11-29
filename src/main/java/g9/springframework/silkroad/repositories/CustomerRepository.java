0a788ee61a301a98a3374fecf188de80e49cef09
pnicto <thehulk489@gmail.com>
2022-11-29 13:24:59 +0530

    Create delete by email method

--- src/main/java/g9/springframework/silkroad/repositories/CustomerRepository.java
+++ src/main/java/g9/springframework/silkroad/repositories/CustomerRepository.java
@@ -2,13 +2,12 @@ package g9.springframework.silkroad.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import g9.springframework.silkroad.models.Customer;
 
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
   Optional<Customer> findByEmail(String email);
 
+  void deleteByEmail(String email);
 }
