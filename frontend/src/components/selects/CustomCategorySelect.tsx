a36ab34be4870b0702f757ea780402439e56b072
pnicto <thehulk489@gmail.com>
2022-11-29 12:13:55 +0530

    Init custom category select

---
+++ frontend/src/components/selects/CustomCategorySelect.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CustomCategorySelect = (props: Props) => {
+  return (
+    <div>CustomCategorySelect</div>
+  )
+}
+
+export default CustomCategorySelect
