578788c672843b43a5fb9980f627ca1d15594c52
pnicto <thehulk489@gmail.com>
2022-11-29 13:24:03 +0530

    Finish delete user functionality

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -1,18 +1,42 @@
 import { Button, Center, Group, Loader } from "@mantine/core";
-import { openModal } from "@mantine/modals";
+import { openConfirmModal, openModal } from "@mantine/modals";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CustomerProfile from "../components/customerProfile/CustomerProfile";
 import OrderHistory from "../components/customerProfile/OrderHistory";
 import ChangePassword from "../components/modals/ChangePassword";
 import TopupWallet from "../components/modals/TopupWallet";
 import { useProfileDataQuery } from "../hooks/useProfileDataQuery";
+import { useLoginStore } from "../store/useLoginStore";
+import { useNotificationStore } from "../store/useNotificationStore";
 
 const UserProfile = () => {
   const profileDataQuery = useProfileDataQuery();
 
   type profileState = "profile" | "orderHistory";
+  const navigate = useNavigate();
+  const loginStore = useLoginStore();
 
   const [activeOption, setActiveOption] = useState<profileState>("profile");
+  const notificationStore = useNotificationStore();
+  const deleteAccountMutation = useMutation(
+    () => axios.delete(`${import.meta.env.VITE_APP_BACKEND_URL}/customers`),
+    {
+      onSuccess: () => {
+        notificationStore.successNotification("Account deleted successfully");
+        loginStore.logoutUser();
+        navigate("/");
+      },
+      onError: (data: AxiosError) => {
+        notificationStore.errorNotification(
+          data.message,
+          `Could not delete account`
+        );
+      },
+    }
+  );
 
   if (profileDataQuery.isLoading) {
     return (
@@ -72,6 +96,30 @@ const UserProfile = () => {
           type="button"
         >
           Change password
+        </Button>
+        <Button
+          type="button"
+          size="md"
+          variant="outline"
+          color={"red"}
+          onClick={() => {
+            openConfirmModal({
+              centered: true,
+              title: "Are you sure?",
+              labels: {
+                confirm: "Confirm",
+                cancel: "Cancel",
+              },
+              confirmProps: {
+                color: "red",
+              },
+              onConfirm: () => {
+                deleteAccountMutation.mutate();
+              },
+            });
+          }}
+        >
+          Delete account
         </Button>
       </Group>
       {activeOption === "profile" && <CustomerProfile />}
