7458d995ac94feb55aeeed7b66af8e6c7e50a34c
pnicto <thehulk489@gmail.com>
2022-11-30 09:39:39 +0530

    Finish form

--- frontend/src/components/forms/AddManagerForm.tsx
+++ frontend/src/components/forms/AddManagerForm.tsx
@@ -1,5 +1,63 @@
+import { Button, Group, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
 const AddManagerForm = () => {
-  return <div>AddManagerForm</div>;
+  const form = useForm({
+    initialValues: {
+      email: "ram@email.com",
+      password: "password",
+      name: "",
+      phoneNumber: "",
+    },
+    validate: {
+      email: (value: string) =>
+        /^\S+@\S+$/.test(value) ? null : "Invalid email",
+      password: (value: string) =>
+        value.trim().length < 4 ? "Invalid password" : null,
+    },
+  });
+
+  return (
+    <form onSubmit={form.onSubmit((values) => {})}>
+      <TextInput
+        withAsterisk
+        label="Name"
+        {...form.getInputProps("name")}
+        type={"text"}
+        py="sm"
+        size="md"
+      />
+      <TextInput
+        withAsterisk
+        label="Phone Number"
+        placeholder="944143256"
+        {...form.getInputProps("phoneNumber")}
+        type={"text"}
+        py="sm"
+        size="md"
+      />
+      <TextInput
+        withAsterisk
+        label="Email"
+        {...form.getInputProps("email")}
+        type="email"
+        py="sm"
+        size="md"
+      />
+      <TextInput
+        withAsterisk
+        label="Password"
+        placeholder="**********"
+        {...form.getInputProps("password")}
+        type="password"
+        py="sm"
+        size="md"
+      />
+      <Group position="right">
+        <Button type="submit">Submit</Button>
+      </Group>
+    </form>
+  );
 };
 
 export default AddManagerForm;
