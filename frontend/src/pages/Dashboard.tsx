7b48d1b4abfd6a0f4dabba42528e44d7906ee33a
pnicto <thehulk489@gmail.com>
2022-11-30 10:35:07 +0530

    Add authority check for manage managers

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,26 +1,29 @@
 import { Button, Group } from "@mantine/core";
 import { useState } from "react";
 import ManageManagers from "../components/dashboard/ManageManagers";
+import { useLoginStore } from "../store/useLoginStore";
 
 const Dashboard = () => {
   type dashboardState = "manage managers" | "reports";
-
+  const { authority } = useLoginStore();
   const [activeOption, setActiveOption] =
     useState<dashboardState>("manage managers");
 
   return (
     <>
       <Group position={"center"} m={"lg"}>
-        <Button
-          onClick={() => {
-            setActiveOption("manage managers");
-          }}
-          variant="outline"
-          size="md"
-          type="button"
-        >
-          Manage managers
-        </Button>
+        {authority === "ADMIN" && (
+          <Button
+            onClick={() => {
+              setActiveOption("manage managers");
+            }}
+            variant="outline"
+            size="md"
+            type="button"
+          >
+            Manage managers
+          </Button>
+        )}
         <Button
           onClick={() => {
             setActiveOption("reports");
