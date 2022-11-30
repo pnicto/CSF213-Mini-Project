127c8e16ae25a0272440d39a67955ac49ae6c271
pnicto <thehulk489@gmail.com>
2022-11-30 21:54:42 +0530

    Convert Crud repos to JPA repos

--- src/main/java/g9/springframework/silkroad/repositories/ManagerRepository.java
+++ src/main/java/g9/springframework/silkroad/repositories/ManagerRepository.java
@@ -2,13 +2,13 @@ package g9.springframework.silkroad.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import g9.springframework.silkroad.models.Manager;
 
 @Repository
-public interface ManagerRepository extends CrudRepository<Manager, Long> {
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
   Optional<Manager> findByEmail(String email);
 
 }
--- src/main/java/g9/springframework/silkroad/repositories/ProductRepository.java
+++ src/main/java/g9/springframework/silkroad/repositories/ProductRepository.java
@@ -1,11 +1,11 @@
 package g9.springframework.silkroad.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import g9.springframework.silkroad.models.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
