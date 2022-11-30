5b74228e98cef53b36daab472ee572fd1261ace3
pnicto <thehulk489@gmail.com>
2022-11-30 08:24:55 +0530

    Add dashboard state, btn on click listeners

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,46 @@
+import { Button, Group } from "@mantine/core";
+import { useState } from "react";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  type dashboardState = "manage managers" | "manage categories" | "reports";
+
+  const [activeOption, setActiveOption] =
+    useState<dashboardState>("manage managers");
+
+  return (
+    <Group position={"center"} m={"lg"}>
+      <Button
+        onClick={() => {
+          setActiveOption("manage managers");
+        }}
+        variant="outline"
+        size="md"
+        type="button"
+      >
+        Manage managers
+      </Button>
+      <Button
+        onClick={() => {
+          setActiveOption("manage categories");
+        }}
+        variant="outline"
+        size="md"
+        type="button"
+      >
+        Manage categories
+      </Button>
+      <Button
+        onClick={() => {
+          setActiveOption("reports");
+        }}
+        variant="outline"
+        size="md"
+        type="button"
+      >
+        Generate reports
+      </Button>
+    </Group>
+  );
 };
 
 export default Dashboard;
