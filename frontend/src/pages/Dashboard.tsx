bff7f2579debf4ccdf4eeaab1ff7dfe1784a9ddb
pnicto <thehulk489@gmail.com>
2022-11-30 08:27:21 +0530

    Add manage managers component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { Button, Group } from "@mantine/core";
 import { useState } from "react";
+import ManageManagers from "../components/dashboard/ManageManagers";
 
 const Dashboard = () => {
   type dashboardState = "manage managers" | "manage categories" | "reports";
@@ -8,38 +9,41 @@ const Dashboard = () => {
     useState<dashboardState>("manage managers");
 
   return (
-    <Group position={"center"} m={"lg"}>
-      <Button
-        onClick={() => {
-          setActiveOption("manage managers");
-        }}
-        variant="outline"
-        size="md"
-        type="button"
-      >
-        Manage managers
-      </Button>
-      <Button
-        onClick={() => {
-          setActiveOption("manage categories");
-        }}
-        variant="outline"
-        size="md"
-        type="button"
-      >
-        Manage categories
-      </Button>
-      <Button
-        onClick={() => {
-          setActiveOption("reports");
-        }}
-        variant="outline"
-        size="md"
-        type="button"
-      >
-        Generate reports
-      </Button>
-    </Group>
+    <>
+      <Group position={"center"} m={"lg"}>
+        <Button
+          onClick={() => {
+            setActiveOption("manage managers");
+          }}
+          variant="outline"
+          size="md"
+          type="button"
+        >
+          Manage managers
+        </Button>
+        <Button
+          onClick={() => {
+            setActiveOption("manage categories");
+          }}
+          variant="outline"
+          size="md"
+          type="button"
+        >
+          Manage categories
+        </Button>
+        <Button
+          onClick={() => {
+            setActiveOption("reports");
+          }}
+          variant="outline"
+          size="md"
+          type="button"
+        >
+          Generate reports
+        </Button>
+      </Group>
+      {activeOption === "manage managers" && <ManageManagers />}
+    </>
   );
 };
 
