226b67be19ba909c4a63a8faac9a2494bd53312f
pnicto <thehulk489@gmail.com>
2022-12-01 21:34:28 +0530

    Add custom query to get total revenue

--- src/main/java/g9/springframework/silkroad/repositories/OrderRepository.java
+++ src/main/java/g9/springframework/silkroad/repositories/OrderRepository.java
@@ -1,9 +1,12 @@
 package g9.springframework.silkroad.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import g9.springframework.silkroad.models.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+  @Query(value = "SELECT SUM(total_price) FROM orders", nativeQuery = true)
+  Double getTotalRevenue();
 
 }
