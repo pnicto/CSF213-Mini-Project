b39bf41aaa42d918a98e0f3fbf1a26aa24f7aff3
pnicto <thehulk489@gmail.com>
2022-12-01 21:36:13 +0530

    Add query to get total products sold in 1 month

--- src/main/java/g9/springframework/silkroad/repositories/OrderRepository.java
+++ src/main/java/g9/springframework/silkroad/repositories/OrderRepository.java
@@ -1,5 +1,7 @@
 package g9.springframework.silkroad.repositories;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +10,8 @@ import g9.springframework.silkroad.models.Order;
 public interface OrderRepository extends JpaRepository<Order, Long> {
   @Query(value = "SELECT SUM(total_price) FROM orders", nativeQuery = true)
   Double getTotalRevenue();
+
+  @Query(value = "SELECT SUM(total_quantity) FROM orders WHERE created_at >= ?1", nativeQuery = true)
+  Integer getTotalProductsSold(LocalDateTime localDateTime);
 
 }
