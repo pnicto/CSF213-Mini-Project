e207b7711ad488089be7ee68c9b385d7d3a36b18
pnicto <thehulk489@gmail.com>
2022-12-02 23:38:21 +0530

    Finish charts

--- frontend/src/components/dashboard/Reports.tsx
+++ frontend/src/components/dashboard/Reports.tsx
@@ -1,15 +1,105 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { ReportsInterface } from "../../types/interfaces";
+import { Container, Paper, Title } from "@mantine/core";
+import { useMemo } from "react";
+import { AxisOptions, Chart } from "react-charts";
+import { LastSixMonth, ReportsInterface } from "../../types/interfaces";
 
-const Reports = () => {
-  const reportsQuery = useQuery(["reports"], () =>
-    axios.get<ReportsInterface>(
-      `${import.meta.env.VITE_APP_BACKEND_URL}/reports`
-    )
+type Props = {
+  data: ReportsInterface;
+};
+
+const Reports = ({ data }: Props) => {
+  const productsSoldData = useMemo(
+    () => [
+      {
+        label: "Products sold",
+        data: data.totalProductsSoldLastSixMonths,
+      },
+    ],
+    []
   );
 
-  return <div>Reports</div>;
+  const revenueData = useMemo(
+    () => [
+      {
+        label: "Revenue generated",
+        data: data.totalRevenueLastSixMonths,
+      },
+    ],
+    []
+  );
+
+  const customersData = useMemo(
+    () => [
+      {
+        label: "Customers registered",
+        data: data.newlyRegisteredCustomersLastSixMonths,
+      },
+    ],
+    []
+  );
+
+  const primaryAxis = useMemo(
+    (): AxisOptions<LastSixMonth> => ({
+      getValue: (datum) => datum.label,
+    }),
+    []
+  );
+
+  const secondaryAxes = useMemo(
+    (): AxisOptions<LastSixMonth>[] => [
+      {
+        getValue: (datum) => {
+          if (datum.value) {
+            return datum.value;
+          } else {
+            return 0;
+          }
+        },
+      },
+    ],
+    []
+  );
+
+  return (
+    <>
+      <Container>
+        <Title order={2}>Products sold in last 6 months</Title>
+        <Paper mih={300} shadow={"md"} withBorder>
+          <Chart
+            options={{
+              data: productsSoldData,
+              primaryAxis,
+              secondaryAxes,
+            }}
+          />
+        </Paper>
+        <Title order={2} mt={"md"}>
+          Revenue generated in last 6 months
+        </Title>
+        <Paper mih={300} shadow={"md"} withBorder>
+          <Chart
+            options={{
+              data: revenueData,
+              primaryAxis,
+              secondaryAxes,
+            }}
+          />
+        </Paper>
+        <Title order={2} mt={"md"}>
+          New customers registered in last 6 months
+        </Title>
+        <Paper mih={300} shadow={"md"} withBorder>
+          <Chart
+            options={{
+              data: customersData,
+              primaryAxis,
+              secondaryAxes,
+            }}
+          />
+        </Paper>
+      </Container>
+    </>
+  );
 };
 
 export default Reports;
