9c7e3f9ca6ea8c1c5d8eed25a13fe9c5903e0e46
pnicto <thehulk489@gmail.com>
2022-12-02 21:13:09 +0530

    Add queries to get revenue,sales b/w 2 dates

--- src/main/java/g9/springframework/silkroad/repositories/OrderRepository.java
+++ src/main/java/g9/springframework/silkroad/repositories/OrderRepository.java
@@ -11,7 +11,13 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
   @Query(value = "SELECT SUM(total_price) FROM orders", nativeQuery = true)
   Double getTotalRevenue();
 
-  @Query(value = "SELECT SUM(total_quantity) FROM orders WHERE created_at >= ?1", nativeQuery = true)
-  Integer getTotalProductsSold(LocalDateTime localDateTime);
+  @Query(value = "SELECT SUM(total_price) FROM orders WHERE created_at BETWEEN ?1 AND ?2", nativeQuery = true)
+  Double getTotalRevenueBetween(LocalDateTime sLocalDateTime, LocalDateTime enLocalDateTime);
+
+  @Query(value = "SELECT SUM(total_quantity) FROM orders", nativeQuery = true)
+  Integer getTotalProductsSold();
+
+  @Query(value = "SELECT SUM(total_quantity) FROM orders WHERE created_at BETWEEN ?1 AND ?2", nativeQuery = true)
+  Integer getTotalProductsSoldBetween(LocalDateTime sLocalDateTime, LocalDateTime enLocalDateTime);
 
 }
