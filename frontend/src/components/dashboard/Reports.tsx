0f366b403bfbe1b0d363d0e5c2b4bb046694b6a6
pnicto <thehulk489@gmail.com>
2022-12-02 21:23:32 +0530

    Add reports query

--- frontend/src/components/dashboard/Reports.tsx
+++ frontend/src/components/dashboard/Reports.tsx
@@ -1,4 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { ReportsInterface } from "../../types/interfaces";
+
 const Reports = () => {
+  const reportsQuery = useQuery(["reports"], () =>
+    axios.get<ReportsInterface>(
+      `${import.meta.env.VITE_APP_BACKEND_URL}/reports`
+    )
+  );
+
   return <div>Reports</div>;
 };
 
