4d65424df717f3ef29913be64abe8838a06493c6
pnicto <thehulk489@gmail.com>
2022-12-06 20:43:13 +0530

    Add route to get all customers

--- src/main/java/g9/springframework/silkroad/controllers/CustomerController.java
+++ src/main/java/g9/springframework/silkroad/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CustomerController {
   private final CustomerRepository customerRepository;
+
+  @GetMapping("/all")
+  Iterable<Customer> getAllCustomers() {
+    return customerRepository.findAll();
+  }
 
   @GetMapping
   Customer getCustomer(Principal principal) {
